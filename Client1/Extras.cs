286640cae4899509f9db928950935c5b6a85c36d
Zachary <reZach@users.noreply.github.com>
2018-11-15 20:48:44 -0600

    Initial commit

---
+++ Client1/Extras.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Extras
+{
+    public enum Message
+    {
+        FAILURE,
+        CLIENT_CONNECT,
+        SERVER_CONNECT
+    }
+
+    public static class TcpListenerExtensions
+    {
+        public static Message ListenForMessage(this TcpListener _)
+        {
+            Message retVal;
+            bool success = false;
+
+            using (TcpClient client = _.AcceptTcpClient())
+            using (NetworkStream ns = client.GetStream())
+            {
+                byte[] response = new byte[1024];
+                int bytesRead = ns.Read(response, 0, response.Length);
+
+                success = Enum.TryParse(Encoding.ASCII.GetString(response, 0, bytesRead), out retVal);
+            }
+
+            if (success)
+                return retVal;
+            else
+                return Message.FAILURE;
+        }
+    }
+
+    public static class NetworkHelpers
+    {
+        public static void SendMessage(string IP, int port, Message message)
+        {
+            using (TcpClient client = new TcpClient(IP, port))
+            using (NetworkStream ns = client.GetStream())
+            {
+                byte[] byteMessage = Encoding.ASCII.GetBytes(message.ToString());
+
+                ns.Write(byteMessage, 0, byteMessage.Length);
+            }
+        }
+    }
+
+    public static class GlobalHelpers
+    {
+        public static void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void PrintGameHeader(string IP, bool cls = false)
+        {
+            if (cls)
+                Console.Clear();
+
+            Print("Welcome to Packet Battle");
+            Print("------------------------");
+            Print($"Your IP is: [{IP}]");
+            Print("");
+            Print("");
+        }
+
+        public static string GetInput(string prompt = null, bool singleKey = false)
+        {
+            if (!string.IsNullOrEmpty(prompt))
+                Print(prompt);
+
+            if (singleKey)
+                return Console.ReadKey().KeyChar.ToString();
+            else
+                return Console.ReadLine();
+        }
+    }
+}
--- Client1/Program.cs
+++ Client1/Program.cs
@@ -1,12 +1,97 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using static Extras.TcpListenerExtensions;
+using static Extras.NetworkHelpers;
+using static Extras.GlobalHelpers;
+using Extras;
 
-namespace Client1
+namespace Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char input = 'a';
+            int port = 6000;
+            int theirPort = 6001;
+            bool running = true;
+            string myIP = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+            string theirIP = string.Empty;
+            Message message;
+            TcpListener listener = new TcpListener(IPAddress.Parse(myIP), port);
+
+            try
+            {                
+                while (running)
+                {
+                    PrintGameHeader(myIP, true);
+                    Print("Please select an option:");
+                    Print("1. Initiate battle (client).");
+                    Print("2. Invite challengers (server).");
+                    Print("3. Exit.");
+                    input = Console.ReadKey().KeyChar;
+                    Console.Clear();
+
+                    switch (input)
+                    {
+                        // The 'client';
+                        // attempts to connect
+                        case '1':
+                            {
+                                PrintGameHeader(myIP, true);
+                                listener.Start();
+
+                                theirIP = GetInput("Enter in the IP (v6) of your opponent:");
+
+                                Print($"Attempting to connect to [{theirIP}]...");
+                                SendMessage(theirIP, theirPort, Message.CLIENT_CONNECT);
+
+                                message = listener.ListenForMessage();
+                                if (message == Message.SERVER_CONNECT)
+                                {
+                                    Print("Success");
+                                }
+                            }
+                            
+                            break;
+
+                        // The 'server';
+                        // waits for a connection
+                        case '2':
+                            {
+                                PrintGameHeader(myIP, true);
+                                listener.Start();
+
+                                Print("Waiting for challengers...");
+                                message = listener.ListenForMessage();
+                                
+                                if (message == Message.CLIENT_CONNECT)
+                                {
+                                    Print("Success!");
+
+                                    SendMessage(theirIP, theirPort, Message.SERVER_CONNECT);
+                                }
+                            }
+                            
+                            break;
+                        default:
+                            running = false;
+                            break;
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+
+            Print("done");
+            Console.ReadKey();
         }
     }
 }
