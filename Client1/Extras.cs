0ffc048f12eda2f08ddb3f364b80ea91d1f34d94
Zachary <reZach@users.noreply.github.com>
2018-11-18 00:36:39 -0600

    Adding comments for more clarity

--- Client1/Extras.cs
+++ Client1/Extras.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Extras
 {
+    /// <summary>
+    ///     An enumeration of the type of <see cref="Message"/>.
+    /// </summary>
     public enum Message
     {
         FAILURE,
@@ -11,19 +14,34 @@ namespace Extras
         SERVER_CONNECT
     }
 
+    /// <summary>
+    ///     Extension methods for <see cref="TcpListener"/>.
+    /// </summary>
     public static class TcpListenerExtensions
     {
+        /// <summary>
+        ///     Wait, and return the response of a client's TcpClient
+        ///     message that is of type <see cref="Message"/>.
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
         public static Message ListenForMessage(this TcpListener _)
         {
             Message retVal;
             bool success = false;
 
+            // Accept a new TCP request and
+            // reference it's network stream
             using (TcpClient client = _.AcceptTcpClient())
             using (NetworkStream ns = client.GetStream())
             {
+                // Read bytes from the network stream
                 byte[] response = new byte[1024];
                 int bytesRead = ns.Read(response, 0, response.Length);
 
+                // The use of the "out" keyword passes retVal by reference
+                // which assigns it to the Message type the response parses out to
+                // if the parse is successful
                 success = Enum.TryParse<Message>(Encoding.ASCII.GetString(response, 0, bytesRead), out retVal);
             }
 
@@ -33,16 +51,26 @@ namespace Extras
                 return Message.FAILURE;
         }
 
+        /// <summary>
+        ///     Wait, and return the response of a client's TcpClient
+        ///     message that is of type <see cref="char[]"/>.
+        /// </summary>
+        /// <param name="_"></param>
+        /// <returns></returns>
         public static char[] ListenForCharArray(this TcpListener _)
         {
             char[] retVal = new char[0];
 
+            // Accept a new TCP request and
+            // reference it's network stream
             using (TcpClient client = _.AcceptTcpClient())
             using (NetworkStream ns = client.GetStream())
             {
+                // Read bytes from the network stream
                 byte[] response = new byte[1024];
                 int bytesRead = ns.Read(response, 0, response.Length);
 
+                // Return the value from the network stream
                 retVal = Encoding.ASCII.GetString(response, 0, bytesRead).ToCharArray();
             }
 
@@ -50,8 +78,18 @@ namespace Extras
         }
     }
 
+    /// <summary>
+    ///     A collection of methods that are used to send
+    ///     messages through a <see cref="TcpClient"/>.
+    /// </summary>
     public static class NetworkHelpers
     {
+        /// <summary>
+        ///     Send a given <see cref="Message"/> to an IP/port.
+        /// </summary>
+        /// <param name="IP"></param>
+        /// <param name="port"></param>
+        /// <param name="message"></param>
         public static void SendMessage(string IP, int port, Message message)
         {
             using (TcpClient client = new TcpClient(IP, port))
@@ -62,6 +100,12 @@ namespace Extras
             }
         }
 
+        /// <summary>
+        ///     Send a given <see cref="char[]"/> to an IP/port.
+        /// </summary>
+        /// <param name="IP"></param>
+        /// <param name="port"></param>
+        /// <param name="message"></param>
         public static void SendCharArray(string IP, int port, char[] charArray)
         {
             using (TcpClient client = new TcpClient(IP, port))
@@ -73,8 +117,19 @@ namespace Extras
         }
     }
 
+    /// <summary>
+    ///     Helper methods used within the logic
+    ///     of the game (Packet Battle)
+    /// </summary>
     public static class GameHelpers
     {
+        /// <summary>
+        ///     Returns a random list of 5 characters,
+        ///     including the character that is passed into
+        ///     this function.
+        /// </summary>
+        /// <param name="myChoice"></param>
+        /// <returns></returns>
         public static char[] CompileChoices(char myChoice)
         {
             Random random = new Random();
@@ -83,9 +138,12 @@ namespace Extras
 
             for (int i = 0; i < retVal.Length; i++)
             {
+                // If our character choice is not a letter
                 if ((i == 0 && ((int)myChoice < 96 || (int)myChoice > 122))
                     || i > 0)
                 {
+                    // Assign temp to a character that doesn't
+                    // already exist in retVal
                     while (retVal.Exists(temp))
                     {
                         temp = (char)random.Next(97, 123); // a-z
@@ -96,13 +154,20 @@ namespace Extras
                 }
             }
 
+            // Shuffle our array
             retVal.Shuffle();
 
             return retVal;
         }
 
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="array"></param>
         private static void Shuffle(this char[] array)
         {
+            // A C# implementation of the
+            // Fisher-Yates shuffling algorithm
             Random rng = new Random();
 
             int n = array.Length;
@@ -116,6 +181,13 @@ namespace Extras
             }
         }
 
+        /// <summary>
+        ///     An extension method that returns true if <paramref name="valueExists"/>
+        ///     exists in <paramref name="array"/>.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="valueExists"></param>
+        /// <returns></returns>
         public static bool Exists(this char[] array, char valueExists)
         {
             for (int i = 0; i < array.Length; i++)
@@ -128,13 +200,26 @@ namespace Extras
         }
     }
 
+    /// <summary>
+    ///     Global functions that abstract low-level functions
+    ///     that print text and/or take in input from the user.
+    /// </summary>
     public static class GlobalHelpers
     {
+        /// <summary>
+        ///     Print information to the console.
+        /// </summary>
+        /// <param name="text"></param>
         public static void Print(string text)
         {
             Console.WriteLine(text);
         }
 
+        /// <summary>
+        ///     Print the game header.
+        /// </summary>
+        /// <param name="IP"></param>
+        /// <param name="cls"></param>
         public static void PrintGameHeader(string IP, bool cls = false)
         {
             if (cls)
@@ -147,6 +232,9 @@ namespace Extras
             Print("");
         }
 
+        /// <summary>
+        ///     Print the rules for the game.
+        /// </summary>
         public static void PrintRules()
         {
             Console.Clear();
@@ -165,6 +253,13 @@ namespace Extras
             Console.Clear();
         }
 
+        /// <summary>
+        ///     Get input from the user.
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="singleKey"></param>
+        /// <param name="intercept"></param>
+        /// <returns></returns>
         public static string GetInput(string prompt = null,
             bool singleKey = false,
             bool intercept = false)
@@ -172,6 +267,8 @@ namespace Extras
             if (!string.IsNullOrEmpty(prompt))
                 Print(prompt);
 
+            // Support getting input for a single key
+            // or an entire line of text
             if (singleKey)
                 return Console.ReadKey(intercept).KeyChar.ToString();
             else
